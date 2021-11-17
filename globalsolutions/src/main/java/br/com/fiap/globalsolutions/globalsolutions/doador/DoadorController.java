4522c9e010722d54b31b247490d25cb61abf7ee9
FillipiGS <fillipi.silva@outlook.com>
2021-11-17 17:50:42 -0300

    Adicionado funções de alteração e exclusão

--- globalsolutions/src/main/java/br/com/fiap/globalsolutions/globalsolutions/doador/DoadorController.java
+++ globalsolutions/src/main/java/br/com/fiap/globalsolutions/globalsolutions/doador/DoadorController.java
@@ -2,9 +2,9 @@ package br.com.fiap.globalsolutions.globalsolutions.doador;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 public class DoadorController {
@@ -28,9 +28,35 @@ public class DoadorController {
         return "/doadores/form";
     }
 
+    @GetMapping("/doadores/{id}")
+    public String alterarDoador(@PathVariable("id") long id, Model model){
+
+        Optional<Doador> doadorOptional = doadorRepository.findById(id);
+
+        if (doadorOptional.isEmpty()) {
+            throw new IllegalArgumentException("Esse doador não existe!");
+        }
+
+        model.addAttribute("doador", doadorOptional.get());
+        return "/doadores/form";
+
+    }
+
     @PostMapping("/doadores/salvar")
     public String salvarDoador(@ModelAttribute("doador") Doador doador){
         doadorRepository.save(doador);
+        return "redirect:/doadores";
+    }
+
+    @GetMapping("/doadores/excluir/{id}")
+    public String deletarDoador(@PathVariable("id") long id){
+        Optional<Doador> doadorOptional = doadorRepository.findById(id);
+
+        if (doadorOptional.isEmpty()) {
+            throw new IllegalArgumentException("Esse doador não existe!");
+        }
+
+        doadorRepository.delete(doadorOptional.get());
         return "redirect:/doadores";
     }
 
